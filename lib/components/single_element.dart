d300d05cc31ab99cc9cf6b8561dc1397bbd2f456
toxirjon96 <tatu.toxirjon@gmail.com>
2023-08-27 22:28:39 +0500

    single element class modified

--- lib/components/single_element.dart
+++ lib/components/single_element.dart
@@ -1,5 +1,6 @@
 import 'package:exam_app/exam_app_library.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SingleElement extends StatefulWidget {
   final Color mainColor;
@@ -79,11 +80,105 @@ class _SingleElementState extends State<SingleElement> {
           ),
           Expanded(
             flex: 8,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset("assets/images/series-3.2.png"),
-              ],
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 50),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Align(
+                    alignment: Alignment.center,
+                    child: Image.asset(
+                      "assets/images/series-3.2.png",
+                      height: 300,
+                    ),
+                  ),
+                  Text(
+                    "Series 3",
+                    style: GoogleFonts.poppins(
+                      fontSize: 24,
+                      color: widget.mainColor,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    "The Apple watch series 3 is designed for people who want to keep their fitness game on point.",
+                    style: GoogleFonts.poppins(
+                      fontSize: 10,
+                      color: widget.additionalColor,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        "\$295",
+                        style: GoogleFonts.poppins(
+                          fontSize: 22,
+                          color: widget.mainColor,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      OutlinedButton(
+                        onPressed: () {},
+                        style: OutlinedButton.styleFrom(
+                          shape: const CircleBorder(),
+                        ),
+                        child: Icon(
+                          Icons.add,
+                          color: widget.mainColor,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  OutlinedButton(
+                    onPressed: () {},
+                    style: OutlinedButton.styleFrom(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 50,
+                        vertical: 15,
+                      ),
+                      backgroundColor: widget.mainColor,
+                    ),
+                    child: Text(
+                      "PROCEED TO BUY",
+                      style: GoogleFonts.poppins(
+                        fontSize: 16,
+                        color: widget.subColor,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  OutlinedButton(
+                    onPressed: () {},
+                    style: OutlinedButton.styleFrom(
+                      side: BorderSide(width: 2.0, color: widget.mainColor),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 50,
+                        vertical: 15,
+                      ),
+                      backgroundColor: widget.subColor,
+                    ),
+                    child: Text(
+                      "ADD TO CART",
+                      style: GoogleFonts.poppins(
+                        fontSize: 16,
+                        color: widget.mainColor,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
